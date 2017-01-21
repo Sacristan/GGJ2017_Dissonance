6b2cf1e9688bf75cdf5cdf81ff67b6f2dd08d41d
Sacristan <girts.kesteris@gmail.com>
2017-01-21 23:21:39 +0200

    Score system basis done

---
+++ Assets/Scripts/Gameplay/ScoreTable.cs
@@ -0,0 +1,12 @@
+﻿public static class ScoreTable
+{
+    public static int KilledNestScore = 10;
+    public static int KilledRangedMobScore = 10;
+    public static int KilledMeeleeMobScore = 10;
+    public static int KilledNestMobScore = 10;
+
+    public static int DamagedNestScore = 1;
+    public static int DamagedRangedMobScore = 1;
+    public static int DamagedMeleeMobScore = 1;
+    public static int DamagedNestMobScore = 1;
+}
