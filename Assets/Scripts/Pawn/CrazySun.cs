a096397c36700d30ceeed2f84517975d84ce7bc5
Clover <clovergruff@gmail.com>
2017-01-22 13:46:15 +0200

    Sun, bug, UI shit

--- Assets/Scripts/Pawn/CrazySun.cs
+++ Assets/Scripts/Pawn/CrazySun.cs
@@ -6,22 +6,49 @@ public class CrazySun : MonoBehaviour
 {
 	public Transform sunGraphics;
 	public float twitchRange = 0.1f;
+	public AudioSource screamSource;
+
+    //[SerializeField]
+    //private Transform target;
 
     [SerializeField]
-    private Transform target;
+    private float risingDuration = 3;
 
     [SerializeField]
-    private float speed = 0.5f;
+    private AnimationCurve risingCurve;
 
-    private void Update()
+	private Vector3 offset;
+
+	private void Awake()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * speed);
-    }
+		StartCoroutine(RiseSequence());
+	}
+
+	IEnumerator RiseSequence()
+	{
+		float t = 0;
+
+
+		while(true)
+		{
+			t += Time.deltaTime / risingDuration;
+
+			offset = Vector3.Lerp(new Vector3(0, -3, 0), Vector3.zero, risingCurve.Evaluate(t));
+			screamSource.volume = risingCurve.Evaluate(t);
+
+			if (t >= 1)
+			{
+				break;
+			}
+
+			yield return null;
+		}
+	}
 
     void LateUpdate()
 	{
 		//sunGraphics.rotation = The.gameCamera.transform.rotation;
 		sunGraphics.rotation = Quaternion.LookRotation(transform.localPosition);
-		sunGraphics.localPosition = Random.insideUnitSphere * twitchRange;
+		sunGraphics.localPosition = Random.insideUnitSphere * twitchRange + offset;
 	}
 }
--- Assets/Scripts/UI/GameUI.cs
+++ Assets/Scripts/UI/GameUI.cs
@@ -13,6 +13,9 @@ public class GameUI : MonoBehaviour
 	[Header("Screens")]
 	public GameObject gameOverPage;
 
+	[Header("Other")]
+	public Image fadeImage;
+
 	void Awake()
 	{
 		The.gameUI = this;
@@ -23,6 +26,8 @@ public class GameUI : MonoBehaviour
 		UpdateHealthGraphics();
 		UpdateAmmoGraphics();
 		UpdateScoreGraphics();
+
+		StartCoroutine(FadeIn());
 	}
 
 	public void UpdateHealthGraphics()
@@ -41,5 +46,26 @@ public class GameUI : MonoBehaviour
 	public void GameOver()
 	{
 		gameOverPage.SetActive(true);
+	}
+
+	public IEnumerator FadeIn()
+	{
+		fadeImage.gameObject.SetActive(true);
+		float t = 0;
+
+		while (true)
+		{
+			t += 0.5f * Time.deltaTime;
+
+			fadeImage.color = Color.Lerp(Color.black, new Color(0,0,0,0), t);
+
+			if (t >= 1)
+			{
+				fadeImage.gameObject.SetActive(false);
+				break;
+			}
+
+			yield return null;
+		}
 	}
 }
