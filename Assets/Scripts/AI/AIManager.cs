11ad123ff942b88937782135a1fc9da1d3ae79c9
Sacristan <girts.kesteris@gmail.com>
2017-01-22 05:15:21 +0200

    Spawner WIP

--- Assets/Scripts/AI/AIManager.cs
+++ Assets/Scripts/AI/AIManager.cs
@@ -1,5 +1,26 @@
 ﻿using UnityEngine;
 
+[System.Serializable]
+public class SpawnConfig
+{
+    #region Fields
+    [SerializeField]
+    private float minSpawnTime = 5f;
+
+    [SerializeField]
+    private float maxSpawnTime = 15f;
+
+    [SerializeField]
+    private float spawnRadius = 100f;
+    #endregion
+
+    #region Properties
+    public float MinSpawnTime { get { return minSpawnTime; } }
+    public float MaxSpawnTime { get { return maxSpawnTime; } }
+    public float SpawnRadius { get { return spawnRadius; } }
+    #endregion
+}
+
 public class AIManager : MonoBehaviour
 {
     private static AIManager singletone;
@@ -7,14 +28,35 @@ public class AIManager : MonoBehaviour
     #region Fields
 
     [SerializeField]
-    private SpawnerNestSpawner spawnerNestSpawner;
+    private Spawner spawner;
+
+    [SerializeField]
+    private MeleeMob meleeMob;
+
+    [SerializeField]
+    private RangedMob rangedMob;
 
     [SerializeField]
     private SpawnerNest spawnerNest;
+
+    [SerializeField]
+    private SpawnConfig spawnerNestConfig;
+
+    [SerializeField]
+    private SpawnConfig meleeMobSpawnConfig;
+
+    [SerializeField]
+    private SpawnConfig rangedMobSpawnConfig;
     #endregion
 
     #region Properties
+    public static MeleeMob MeleeMob { get { return singletone.meleeMob; } }
+    public static RangedMob RangedMob { get { return singletone.rangedMob; } }
     public static SpawnerNest SpawnerNest { get { return singletone.spawnerNest; } }
+
+    public static SpawnConfig MeleeMobSpawnConfig { get { return singletone.meleeMobSpawnConfig; } }
+    public static SpawnConfig RangedMobSpawnConfig { get { return singletone.rangedMobSpawnConfig; } }
+    public static SpawnConfig SpawnerNestSpawnConfig { get { return singletone.spawnerNestConfig; } }
     #endregion
 
     #region MonoBehaviour
@@ -23,7 +65,7 @@ public class AIManager : MonoBehaviour
         if (singletone == null) singletone = this;
         else Destroy(singletone);
 
-        SpawnerNestSpawner instance = Instantiate<SpawnerNestSpawner>(spawnerNestSpawner);
+        Spawner instance = Instantiate<Spawner>(spawner);
         instance.transform.position = Vector3.zero;
     }
     #endregion
---
+++ Assets/Scripts/AI/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private static int MAX_MOB_COUNT = 50;
+
+    #region Cache
+    private int _mobCount = 0;
+    #endregion
+
+    private void Awake()
+    {
+        StartCoroutine(SpawnRoutine(AIManager.SpawnerNest.gameObject, AIManager.SpawnerNestSpawnConfig));
+        StartCoroutine(SpawnRoutine(AIManager.MeleeMob.gameObject, AIManager.MeleeMobSpawnConfig));
+        StartCoroutine(SpawnRoutine(AIManager.RangedMob.gameObject, AIManager.RangedMobSpawnConfig));
+    }
+
+    private IEnumerator SpawnRoutine(GameObject objectToSpawn, SpawnConfig spawnConfig)
+    {
+        CalculateMobCount();
+        while (_mobCount > MAX_MOB_COUNT) { yield return new WaitForSeconds(2f); }
+
+        Vector3 spawnPos = AIUtils.FindSuitableRandomPosition(transform.position, spawnConfig.SpawnRadius);
+        Instantiate(objectToSpawn, spawnPos, Quaternion.identity);
+
+        float timeToWait = Random.Range(spawnConfig.MinSpawnTime, spawnConfig.MaxSpawnTime);
+        yield return new WaitForSeconds(timeToWait);
+
+        StartCoroutine(SpawnRoutine(objectToSpawn, spawnConfig));
+    }
+
+    private void CalculateMobCount()
+    {
+        _mobCount = GameObject.FindObjectsOfType<Mob>().Length;
+        Sacristan.Logger.Log(string.Format("Found {0} mobs", _mobCount));
+    }
+
+}
+
