915ff1ed938a2499c66ab720323e06c38dea9b41
Clover <clovergruff@gmail.com>
2017-01-22 13:03:38 +0200

    Sun and Nest shit

---
+++ Assets/Scripts/Pawn/CrazySun.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrazySun : MonoBehaviour
+{
+	public Transform sunGraphics;
+	public float twitchRange = 0.1f;
+
+	void LateUpdate()
+	{
+		//sunGraphics.rotation = The.gameCamera.transform.rotation;
+		sunGraphics.rotation = Quaternion.LookRotation(transform.localPosition);
+		sunGraphics.localPosition = Random.insideUnitSphere * twitchRange;
+	}
+}
