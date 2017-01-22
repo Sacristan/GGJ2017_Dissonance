0d07432398e9d9c08f0aaaf5b65fa3c3f2ca2066
Sacristan <girts.kesteris@gmail.com>
2017-01-22 11:55:05 +0200

    Rocket now uses rigidbody

---
+++ Assets/Scripts/Projectiles/RocketProjectile.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class RocketProjectile : Projectile
+{
+    private Rigidbody rigidBody;
+    private Quaternion initialQuaternion;
+
+    public override void Awake()
+    {
+        base.Awake();
+
+        rigidBody = GetComponent<Rigidbody>();
+        Destroy(gameObject, 10f);
+    }
+
+    public void Start()
+    {
+        rigidBody.velocity = direction * speed;
+        initialQuaternion = rigidBody.rotation;
+    }
+
+    public override void Update()
+    {
+        //base.Update();
+        rigidBody.angularVelocity = Vector3.zero;
+        rigidBody.rotation = initialQuaternion;
+    }
+}
